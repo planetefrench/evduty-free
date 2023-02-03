9f847be016c8477943a7cf303146d8eac2eb9289
planetefrench <68170251+planetefrench@users.noreply.github.com>
2023-02-02 23:05:30 -0500

    Initial API support.

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# evduty-free
+# evdutyfree
  Unofficial Python Module interface for EVduty chargers api
+
+Based on https://github.com/cliviu74/wallbox
+
+## Example
+
+```
+from evdutyfree import EVdutyFree
+
+w = EVdutyFree("user@domain.com", "password")
+
+# Authenticate with the credentials above
+w.authenticate()
+stations = w.get_stations()
+terminals = w.get_terminals(stations[0])
+
+print(w.get_max_charging_current(stations[0],terminals[0]))
+w.set_max_charging_current(stations[0], terminals[0], 30)
+print(w.get_max_charging_current(stations[0],terminals[0]))
+```
---
+++ setup.py
@@ -0,0 +1,24 @@
+import os
+from setuptools import setup
+
+
+def read(name):
+    return open(os.path.join(os.path.dirname(__file__), name)).read()
+
+
+setup(
+    name="evdutyfree",
+    version="0.0.1",
+    description="Unofficial module for interacting with EVDuty EV charger api",
+    long_description=read("README.md"),
+    long_description_content_type="text/markdown",
+    keywords="",
+    author="Jean-Marc G",
+    author_email="none@none.none",
+    url="https://github.com/planetefrench/evduty-free",
+    license="Apache 2",
+    packages=["evdutyfree"],
+    install_requires=["requests>=2.22.0", "simplejson>=3.16.0", "aenum>=3.1.8"],
+    python_requires=">=3.7",
+    classifiers=["Programming Language :: Python :: 3",],
+)
